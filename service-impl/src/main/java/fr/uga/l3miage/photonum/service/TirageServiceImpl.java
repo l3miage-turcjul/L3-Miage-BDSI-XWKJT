9b9d82ed887af896a565e7b9c98e67dbd2b09d66
l3miage-turcjul <julienturc24@gmail.com>
2023-03-27 15:55:57 +0200

    Feat: Services pour Cadre, impression et tirage

--- service-impl/src/main/java/fr/uga/l3miage/photonum/service/TirageServiceImpl.java
+++ service-impl/src/main/java/fr/uga/l3miage/photonum/service/TirageServiceImpl.java
@@ -1,5 +1,48 @@
 package fr.uga.l3miage.photonum.service;
 
-public class TirageServiceImpl {
-    
+import fr.uga.l3miage.photonum.data.domain.Tirage;
+// import fr.uga.l3miage.photonum.data.repo.TirageRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+@Transactional
+public class TirageServiceImpl implements TirageService{
+    private final TirageRepository tirageRepository;
+
+    @Autowired
+    public TirageServiceImpl(TirageRepository tirageRepository){
+        this.tirageRepository = tirageRepository;
+    }
+
+    @Override
+    public Tirage save(Tirage tirage){
+        return tirageRepository.save(tirage);
+    }
+
+    @Override
+    public Tirage get(Long id) throws EntityNotFoundException{
+        return tirageRepository.get(id);
+    }
+
+    @Override
+    public Collection<Tirage> list(){
+        return tirageRepository.all();
+    }
+
+    @Override
+    public Tirage update(Tirage tirage) throws EntityNotFoundException{
+        return tirageRepository.save(tirage);
+    }
+
+    @Override
+    public void delete(Long id) throws EntityNotFoundException{
+        Tirage tirage = get(id);
+        if(tirage == null){
+            throw new EntityNotFoundException("tirage with id=%d not found".formatted(id));
+        }
+        tirageRepository.delete(tirage);
+    }
 }
--- service-pub/src/main/java/fr/uga/l3miage/photonum/service/TirageService.java
+++ service-pub/src/main/java/fr/uga/l3miage/photonum/service/TirageService.java
@@ -1,5 +1,13 @@
 package fr.uga.l3miage.photonum.service;
+import fr.uga.l3miage.photonum.data.domain.Tirage;
+import fr.uga.l3miage.photonum.service.base.BaseService;
 
-public class TirageService {
-    
+import java.util.Collection;
+public interface TirageService extends BaseService<Tirage, Long> {
+    Tirage save(Tirage tirage);
+    void delete(Long id) throws EntityNotFoundException;
+    Tirage get(Long id) throws EntityNotFoundException;
+    Collection<Tirage> list();
+    Tirage update(Tirage tirage) throws EntityNotFoundException;
+
 }
