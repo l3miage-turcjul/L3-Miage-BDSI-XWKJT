c4e369eb255285539be7333b7f2f2680511cc54c
l3miage-jandotdf <floriane-jandot@hotmail.fr>
2023-03-27 14:04:00 +0200

    feat adressePostale, client, photo, image Service&ServiceImpl

--- service-impl/src/main/java/fr/uga/l3miage/photonum/service/PhotoServiceImpl.java
+++ service-impl/src/main/java/fr/uga/l3miage/photonum/service/PhotoServiceImpl.java
@@ -1,5 +1,52 @@
 package fr.uga.l3miage.photonum.service;
+import fr.uga.l3miage.photonum.data.domain.Photo;
+import fr.uga.l3miage.photonum.service.base.BaseService;
+import fr.uga.l3miage.photonum.service.EntityNotFoundException;
 
-public class PhotoServiceImpl {
-    
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PhotoServiceImpl implements PhotoService{
+
+    private final PhotoRepository photoRepository;
+
+    @Autowired
+    public PhotoServiceImpl(PhotoRepository photoRepository) {
+        this.photoRepository = photoRepository;
+    }
+
+
+    @Override
+    public Photo save(Photo cli) throws EntityNotFoundException {
+        return photoRepository.save(cli);
+    }
+
+    @Override
+    public Photo get(Long id) throws EntityNotFoundException {
+        return photoRepository.get(id);
+    }
+
+    @Override
+    public Photo update(Photo pho) throws EntityNotFoundException {
+        return photoRepository.save(pho);
+    }
+
+    @Override
+    public void delete(Long id) throws EntityNotFoundException{
+        Photo pho = get(id);
+        if (pho == null) {
+            throw new EntityNotFoundException("la photo avec id=%d n'a pas été trouvé".formatted(id));
+        }
+
+        photoRepository.delete(pho);
+
+    }
+
+    @Override
+    public Collection<Photo> list() {
+        return photoRepository.all();
+    }
+
 }
+
