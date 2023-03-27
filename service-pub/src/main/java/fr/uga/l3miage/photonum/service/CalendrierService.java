c4013427ec90c910e742236f8553dbb72d793558
l3miage-koneze <zeinaboubissikone@gmail.com>
2023-03-27 16:26:05 +0100

    Fix

--- service-pub/src/main/java/fr/uga/l3miage/photonum/service/CalendrierService.java
+++ service-pub/src/main/java/fr/uga/l3miage/photonum/service/CalendrierService.java
@@ -3,13 +3,10 @@ package fr.uga.l3miage.photonum.service;
 import fr.uga.l3miage.photonum.data.domain.Calendrier;
 import fr.uga.l3miage.photonum.service.CalendrierService;
 import fr.uga.l3miage.photonum.service.base.BaseService;
-import fr.uga.l3miage.photonum.data.repo.CalendrierRepository;
-import jakarta.transaction.Transactional;
+
 
 import java.util.Collection;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 public interface CalendrierService extends BaseService<Calendrier,Long>{
 
