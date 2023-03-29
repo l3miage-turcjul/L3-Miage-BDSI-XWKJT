00c84277336e47ca3427b86fea6ef16986ae37cc
l3miage-jandotdf <floriane-jandot@hotmail.fr>
2023-03-29 19:49:49 +0200

    fix CommandeMapper était pas codé

--- app/src/main/java/fr/uga/l3miage/photonum/Commande/CommandeMapper.java
+++ app/src/main/java/fr/uga/l3miage/photonum/Commande/CommandeMapper.java
@@ -1,8 +1,16 @@
 package fr.uga.l3miage.photonum.Commande;
 
+import java.util.Collection;
+
 import org.mapstruct.Mapper;
+
+import fr.uga.l3miage.photonum.data.domain.Commande;
 
 @Mapper(componentModel = "spring")
 public interface CommandeMapper {
-    
+    CommandeDTO entityToDTO (Commande commande);
+    Collection<CommandeDTO> entityToDTO(Iterable<Commande> commandes);
+
+    Commande dtoToEntity(CommandeDTO commande);
+    Collection<Commande> dtoToEntity(Iterable<CommandeDTO> commandes);
 }
