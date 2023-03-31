127f7031c5d31dcea5c5a03ed11849cf74100186
l3miage-jandotdf <floriane-jandot@hotmail.fr>
2023-03-31 22:01:12 +0200

    feat codage de photoController

--- app/src/main/java/fr/uga/l3miage/photonum/Photo/PhotoController.java
+++ app/src/main/java/fr/uga/l3miage/photonum/Photo/PhotoController.java
@@ -1,5 +1,94 @@
 package fr.uga.l3miage.photonum.Photo;
 
+import java.util.Collection;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import fr.uga.l3miage.photonum.data.domain.Photo;
+import fr.uga.l3miage.photonum.service.PhotoService;
+import fr.uga.l3miage.photonum.service.EntityNotFoundException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping(value = "/api/v1", produces = "application/json")
 public class PhotoController {
-    
+
+    private final PhotoService photoService;
+    private final PhotoMapper photoMapper;
+
+    @Autowired
+    public PhotoController(PhotoService photoService, PhotoMapper photoMapper) {
+        this.photoService = photoService;
+        this.photoMapper = photoMapper;
+    }
+
+    @GetMapping("/Photo")
+    public Collection<PhotoDTO> Photos(@RequestParam(value = "p", required = false) String pays) {
+            return photoMapper.entityToDTO(photoService.list()); 
+    }
+
+    @GetMapping("/Photo/{id}")
+    public PhotoDTO Photo(@PathVariable("id") @NotNull Long id) {
+        try {
+            return photoMapper.entityToDTO(photoService.get(id));
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
+        }
+    }
+
+    @PostMapping("/Client/{id}/Photo")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PhotoDTO newPhoto(@PathVariable("id") @NotNull Long clientId,
+            @RequestBody @Valid PhotoDTO Photo) {
+        try {
+            final var entity = photoService.save(clientId, photoMapper.dtoToEntity(Photo));
+            return photoMapper.entityToDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, null, e);
+        }
+    }
+
+    @PutMapping("/Photo/{id}")
+    public PhotoDTO updatePhoto(@PathVariable("id") @NotNull Long id,
+                                                  @RequestBody @Valid PhotoDTO photo) {
+        try {
+            if (photo.id().equals(id)) {
+                Photo photoEntity = photoMapper.dtoToEntity(photo);
+                var updated = photoService.update(photoEntity);
+                return photoMapper.entityToDTO(updated);
+            }
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, null, e);
+        }
+    }
+
+    @DeleteMapping("/Photo/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePhoto(@PathVariable("id") @NotNull Long id) {
+        try {
+            photoService.delete(photoService.get(id));
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
+        }
+    }
+
 }
