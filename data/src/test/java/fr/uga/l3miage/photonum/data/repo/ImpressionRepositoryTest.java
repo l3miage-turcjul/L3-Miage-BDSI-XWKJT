7abbab3fcb403a7fd3e1cca76f78b006c49444ed
l3miage-xusi <121896780+l3miage-xusi@users.noreply.github.com>
2023-03-31 13:49:22 +0200

    Fix : Fixtures.java

--- data/src/test/java/fr/uga/l3miage/photonum/data/repo/ImpressionRepositoryTest.java
+++ data/src/test/java/fr/uga/l3miage/photonum/data/repo/ImpressionRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 class ImpressionRepositoryTest extends Base {
 
     @Autowired
-    ImpressionRepository authorRepository;
+    ImpressionRepository impressionRepository;
 
     @Test
     void todo() {
