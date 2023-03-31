bb27a85fc97280ec824cbf0d1c1e8f9f62110779
l3miage-jandotdf <floriane-jandot@hotmail.fr>
2023-03-31 22:59:28 +0200

    fix calendrierController

--- app/src/main/java/fr/uga/l3miage/photonum/Calendrier/CalendrierController.java
+++ app/src/main/java/fr/uga/l3miage/photonum/Calendrier/CalendrierController.java
@@ -1,7 +1,7 @@
 package fr.uga.l3miage.photonum.Calendrier;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,11 +15,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import fr.uga.l3miage.photonum.Page.PageDTO;
 import fr.uga.l3miage.photonum.Page.PageMapper;
-import fr.uga.l3miage.photonum.Photo.PhotoDTO;
-import fr.uga.l3miage.photonum.service.CadreService;
+import fr.uga.l3miage.photonum.data.domain.Calendrier;
+import fr.uga.l3miage.photonum.data.domain.Page;
 import fr.uga.l3miage.photonum.service.CalendrierService;
 import fr.uga.l3miage.photonum.service.EntityNotFoundException;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public class CalendrierController {
@@ -36,7 +38,7 @@ public class CalendrierController {
     }
 
     @GetMapping("/Calendrier")
-    public Collection<CalendrierDTO> calendrier() {
+    public Collection<CalendrierDTO> calendriers() {
         return calendrierMapper.entityToDTO(calendrierService.list());
     }
 
@@ -49,9 +51,38 @@ public class CalendrierController {
         }
     }
 
+    @PostMapping("/Article/{id}/Calendrier")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CalendrierDTO newCalendrier(@RequestBody CalendrierDTO calendrier, @PathVariable("id") @NotNull Long calendrierId)
+            throws EntityNotFoundException {
+        try {
+            Calendrier entity = calendrierService.save(calendrierId, calendrierMapper.dtoToEntity(calendrier));
+            return calendrierMapper.entityToDTO(entity);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, null, e);
+        }
+    }
+
+    @PutMapping("/Calendrier/{id}")
+    public CalendrierDTO updateCalendrier(@PathVariable("id") @NotNull Long id,
+                                @RequestBody @Valid CalendrierDTO calendrier) {
+        try {
+            if (calendrierMapper.dtoToEntity(calendrier).getId().equals(id)) {
+                Calendrier calendrierEntity = calendrierMapper.dtoToEntity(calendrier);
+                var updated = calendrierService.update(calendrierEntity);
+                return calendrierMapper.entityToDTO(updated);
+            }
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, null, e);
+        }
+    }
+
     @DeleteMapping("/Calendrier/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCadre(@PathVariable("id") Long id) {
+    public void deleteCalendrier(@PathVariable("id") Long id) {
         try {
             calendrierService.delete(id);
         } catch (EntityNotFoundException e) {
@@ -59,14 +90,15 @@ public class CalendrierController {
         }
     }
 
-    @GetMapping("/Cadre/{id}/Photo")
-    public Collection<PhotoDTO> photos(@PathVariable("id") @NotNull Long id) {
+    @GetMapping("/Calendrier/{id}/Page")
+    public Collection<PageDTO> calendrierPages(@PathVariable("id") @NotNull Long CalendrierId) {
         try {
-            var cadre = cadreService.get(id);
-            List<Photo> photos = cadre.getPhotosDeCadre();
-            return photoMapper.entityToDTO(photos);
+            var Calendrier = calendrierService.get(CalendrierId);
+            Set<Page> pages = Calendrier.getPagesCalendrier();
+            return pageMapper.entityToDTO(pages);
         } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, null, e);
         }
+
     }
 }
