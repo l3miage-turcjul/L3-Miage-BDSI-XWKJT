801a5ff6ec32b9cf5160553e8364f6cf68149fce
l3miage-koneze <zeinaboubissikone@gmail.com>
2023-03-31 22:52:00 +0100

    Fix : Commande

--- app/src/main/java/fr/uga/l3miage/photonum/Commande/CommandeMapper.java
+++ app/src/main/java/fr/uga/l3miage/photonum/Commande/CommandeMapper.java
@@ -4,13 +4,15 @@ import java.util.Collection;
 
 import org.mapstruct.Mapper;
 
+import fr.uga.l3miage.photonum.Client.ClientDTO;
 import fr.uga.l3miage.photonum.data.domain.Commande;
+import jakarta.validation.Valid;
 
 @Mapper(componentModel = "spring")
 public interface CommandeMapper {
     CommandeDTO entityToDTO (Commande commande);
     Collection<CommandeDTO> entityToDTO(Iterable<Commande> commandes);
 
-    Commande dtoToEntity(CommandeDTO commande);
+    Commande dtoToEntity(@Valid @Valid @Valid @Valid CommandeDTO commande);
     Collection<Commande> dtoToEntity(Iterable<CommandeDTO> commandes);
 }
