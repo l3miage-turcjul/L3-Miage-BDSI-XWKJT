20299be1c2839526bd6f8593dc77577fb0281998
l3miage-koneze <zeinaboubissikone@gmail.com>
2023-04-01 10:37:42 +0100

    Fix : ImageDTO

--- app/src/main/java/fr/uga/l3miage/photonum/Image/ImageDTO.java
+++ app/src/main/java/fr/uga/l3miage/photonum/Image/ImageDTO.java
@@ -2,15 +2,29 @@ package fr.uga.l3miage.photonum.Image;
 
 import java.util.Collection;
 
+import fr.uga.l3miage.photonum.Client.ClientDTO;
 import fr.uga.l3miage.photonum.Page.PageDTO;
+import fr.uga.l3miage.photonum.Photo.PhotoDTO;
 import jakarta.validation.constraints.Null;
 
 public record ImageDTO (
+
+    Long id,
+    String cheminAcces,
+    String info,
+    float resolution,
+
+    @Null
+    ClientDTO proprietaire,
+    
+    boolean partage,
+
+    @Null
+    Collection<PhotoDTO> photos,
+
     @Null
     Collection<PageDTO> pagesCalendrier
 )
 {
 
-    public Object id() {
-        return null;
-    }}
+  }
